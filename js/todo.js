0ac0cf05e509d05945cfe0a388fde70921f6473c
artian99 <65194172+artian99@users.noreply.github.com>
2022-02-07 00:44:04 +0900

    220207

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,13 @@
 const todoForm = document.getElementById('todo-form');
 const todoInput = document.querySelector('#todo-form input');
 const todoList = document.getElementById('todo-list');
+const TODOS_KEY = 'todos';
+
+let todos = [];
+
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
 
 function handleTodoSubmit (event) {
     event.preventDefault();
@@ -8,8 +15,16 @@ function handleTodoSubmit (event) {
     // input의 현재 value를 newTodo라는 변수에 복사
 
     todoInput.value = '';
-    paintTodo(newTodo);
+    
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj);
     // 입력값을 paintTodo에 넣어서 호출
+    saveTodos();
 } 
 
 function paintTodo(newTodo) {
@@ -19,22 +34,33 @@ function paintTodo(newTodo) {
     const span = document.createElement('span');
     const button = document.createElement('button');
     button.innerText = '❌';
-    span.innerText = newTodo;
+
+    li.id = newTodo.id;
+    span.innerText = newTodo.text;
 
     button.addEventListener('click', deleteTodo);
     
     li.appendChild(span); // li가 span이라는 자식 요소를 가지게 됨
     li.appendChild(button);
-    
     todoList.appendChild(li); 
 }
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter(todo => todo.id !== parseInt(li.id));
+    saveTodos();
 }
 
 
 todoForm.addEventListener('submit', handleTodoSubmit);
 
-// 지금 문제: 짝수번째 submit을 하면 자꾸 delete 이벤트가 발생함
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if(savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    // parsedTodos array 내부의 item을 가지고 item을 나타내길 원함
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo); 
+    // paintTodo는 텍스트를 받고, JS가 그 텍스트를 paintTodo에 전달
+}
---
+++ js/weather.js
@@ -0,0 +1,24 @@
+API_KEY = 'd81cce3c7a0f38789b456036d05b837e'
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log('You live it', lat, lon);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json()).then(data => {
+            // span에서 날씨/지역을 주는 컨테이너
+            const city = document.querySelector('#weather span:first-child');
+            const weather = document.querySelector('#weather span:last-child');
+
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}˚C`;
+    });
+}
+
+function onGeoError() {
+    alert('can\'t find you. No weather for you')
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
